20e2c17f48f433db8f255927bdabb287e06b842d
SyA <herrysans@yahoo.com>
2025-06-05 13:52:37 +0700

    improve: device uuid update and view update

--- app/Livewire/Auth/Settings/DeviceUuid.php
+++ app/Livewire/Auth/Settings/DeviceUuid.php
@@ -10,7 +10,7 @@ class DeviceUuid extends Component
 {
     use HasApiHelper;
     public $title = 'Pengaturan - Device UUID';
-    public $device_uuid;
+    public $device_uuid, $device_uuid_by_cookie;
     public $original;
     public function boot()
     {
@@ -18,12 +18,26 @@ class DeviceUuid extends Component
     }
     public function refresh()
     {
-        $this->device_uuid = Cookie::get(env('APP_DEVICE_UUID_COOKIES_NAME'));
+        $this->device_uuid_by_cookie = Cookie::get(env('APP_DEVICE_UUID_COOKIES_NAME'));
+        $data = $this->API_getJSON('view/settings/device-uuid')->data;
+        $this->device_uuid = $data->device_uuid;
     }
     public function render()
     {
         return view('livewire.auth.settings.device-uuid')->layout('components.layouts.app', [
             'title' => $this->title
-    ]);;
+        ]);
+    }
+    public function change_device_uuid()
+    {
+        $response = $this->API_postJSON('view/settings/device-uuid/change', [
+            'device_uuid' => $this->device_uuid,
+        ]);
+        if ($response->status != 200) {
+            $this->dispatch('notify', type: 'error', message: $response->data->message);
+            return;
+        }
+        $this->dispatch('notify', type: 'success', message: $response->data->message);
+        $this->refresh();
     }
 }
--- resources/views/livewire/auth/settings/device-uuid.blade.php
+++ resources/views/livewire/auth/settings/device-uuid.blade.php
@@ -14,11 +14,36 @@
                     yang
                     digunakan. Hal ini digunakan untuk login menggunakan pin, push notifikasi, dan lainnya.</p>
             </div>
-            <div>
-                <input disabled wire:model="device_uuid" type="text" id="device_uuid"
-                    class="block p-2.5 mb-3 w-full text-sm text-gray-600 bg-gray-50 rounded-lg border border-gray-300 focus:ring-ocean-500 focus:border-ocean-500">
+            <div class="text-left">
+                <div>
+                    <label for="device_uuid" class="text-xs md:text-base text-gray-500 ">UUID perangkat ini</label>
+                    <input disabled wire:model="device_uuid_by_cookie" type="text" id="device_uuid_by_cookie"
+                        class="mt-2 block p-2.5 mb-3 w-full text-sm text-gray-600 bg-gray-50 rounded-lg border border-gray-300 focus:ring-ocean-500 focus:border-ocean-500">
+                </div>
+                <div>
+                    <label for="device_uuid" class="text-xs md:text-base text-gray-500 ">UUID perangkat yang terpaut
+                        pada
+                        akun ini</label>
+                    <input disabled wire:model="device_uuid" type="text" id="device_uuid"
+                        class="mt-2 block p-2.5 mb-3 w-full text-sm text-gray-600 bg-gray-50 rounded-lg border border-gray-300 focus:ring-ocean-500 focus:border-ocean-500">
+                </div>
 
             </div>
+            <div class="mt-2 border p-3 rounded text-left flex items-center gap-3 bg-gray-50">
+                <i class="bi bi-info-circle text-ocean-500 text-lg md:text-2xl"></i>
+                <p class="text-xs md:text-base text-gray-500">Jika UUID perangkat saat ini tidak sesuai dengan UUID
+                    terpaut pada akun, <br>
+                    <span class="text-ocean-500">"Logout"</span> kemudian saat akan melakukan login, centang <span
+                        class="text-ocean-500">"Ingat pada perangkat"</span>
+                    dan cek
+                    kembali pada halaman ini, atau klik tombol berikut untuk menyesuaikan UUID perangkat.
+                </p>
+            </div>
+            <button wire:click="change_device_uuid"
+                class="my-3 btn btn-outline-ocean w-full min-w-32 py-2 flex gap-2 items-center justify-center">
+                <div wire:loading.remove wire:target="change_device_uuid">Perbaharui UUID perangkat pada akun</div>
+                <div wire:loading wire:target="change_device_uuid" class="text-center small-loader"></div>
+            </button>
         </div>
         {{-- <div class="flex items-center gap-4">
             <i class="bi bi-patch-check-fill text-ocean-600 text-3xl"></i>
